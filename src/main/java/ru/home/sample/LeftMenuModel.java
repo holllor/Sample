35bcf0632fe8fcaece54f244b59428e0efa61acc
олег <@309_evstratovskii>
2016-09-29 14:21:44 +0300

    работа по левому меню, его генерации и получения из БД

---
+++ src/main/java/ru/home/sample/LeftMenuModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.home.sample;
+import java.util.List;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.DynamicMenuModel;
+import org.primefaces.model.menu.MenuElement;
+import org.primefaces.model.menu.MenuModel;
+/**
+ *
+ * @author олег
+ */
+public class LeftMenuModel {
+    private void testMenu(){
+        MenuModel model = new DefaultMenuModel();
+        // MenuModel model1 = new DynamicMenuModel();
+       
+    }
+}
