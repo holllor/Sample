0bc2607644bef2ffcabcb8095c38a876de65396e
олег <@309_evstratovskii>
2016-10-03 12:37:48 +0300

    добавлен драйвер для постгрескюл и начато тестирование для левого меню

--- src/main/java/ru/home/sample/LeftMenuModel.java
+++ src/main/java/ru/home/sample/LeftMenuModel.java
@@ -4,21 +4,54 @@
  * and open the template in the editor.
  */
 package ru.home.sample;
-import java.util.List;
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.DynamicMenuModel;
-import org.primefaces.model.menu.MenuElement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.primefaces.model.menu.MenuModel;
+
 /**
  *
  * @author олег
  */
 public class LeftMenuModel {
-    private void testMenu(){
-        MenuModel model = new DefaultMenuModel();
-        // MenuModel model1 = new DynamicMenuModel();
-       
+
+    public static void main(String[] args) {
+        LeftMenuModel l = new LeftMenuModel();
+        l.testMenu();
+    }
+
+    private void testMenu() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+
+        }
+        String sql = "select * from leftmenu";
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5432/sample", "sample",
+                    "sample");
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            ResultSetMetaData rsmt = rs.getMetaData();
+
+            while (rs.next()) {
+                int i1 = rs.getInt("id");
+                System.out.println(i1);
+            }
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+
+        }
+
     }
 }
