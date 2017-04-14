685902657604358aa87fc15f796587fa444db024
katy <katy@doc>
2017-04-14 12:54:02 +0300

    добавлено новое описание по админским страницам подготовлены файлы контекста и ежб класса для работы на странице вставлена таблица вывода информации по товару(пока упрощенная)

---
+++ src/main/java/ru/home/cdi/LoadInfoCDI.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.home.cdi;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import ru.home.ejb.LoadInfoEJB;
+import ru.home.entity.Tovar;
+
+/**
+ *
+ * @author katy
+ */
+@Named(value = "loadInfoCDI")
+@SessionScoped
+public class LoadInfoCDI implements Serializable {
+
+    private List<Tovar> listTovar;
+    @EJB
+    LoadInfoEJB loadEjb;
+    //метод отрабатывает при загрузке CDI Bean
+
+    @PostConstruct
+    public void init() {
+        listTovar = loadEjb.selectAllTovar(); // загружает модель данных меню
+
+    }
+
+    
+    
+    // getter and setter
+
+    public List<Tovar> getListTovar() {
+        return listTovar;
+    }
+
+    public void setListTovar(List<Tovar> listTovar) {
+        this.listTovar = listTovar;
+    }
+    
+}
---
+++ src/main/java/ru/home/ejb/LoadInfoEJB.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.home.ejb;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import ru.home.entity.Tovar;
+
+/**
+ *
+ * @author katy
+ */
+@Stateless
+public class LoadInfoEJB {
+
+    @PersistenceContext(unitName = "samplePG")
+    private EntityManager em;
+    
+    /**
+     * выбор всего товара из базы
+     * @return лист
+     */
+    public List<Tovar> selectAllTovar(){
+        String sql = "select * from tovar t ";
+        Query qer = em.createNativeQuery(sql, Tovar.class);
+        return qer.getResultList();
+    }
+}
